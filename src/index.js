1cb4d6ed9b7453d3bf7e4842a9f28f50f1e9f569
Jakub Syrek <jksyrek@gmail.com>
2017-03-17 16:52:33 +0100

    YouTubeSearch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDiRd5_9yRXWjFnMvS8KVH2MS7o8LSSUCM';
+
+YTSearch({key: API_KEY, term: 'taco hemingway'}, function(data) {
+				console.log(data);
+});
 
 const App = () => {
   return (
