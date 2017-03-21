fda67bdd49ac370e3ad53ab9735e6d89d6508897
Jakub Syrek <jksyrek@gmai.com>
2017-03-21 11:12:53 +0100

    Display first video from list in iFrame

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
-    const videoId = video.id.video.id;
+  if (!video) {
+    return <div>Loading...</div>
+  }
+
+    const videoId = video.id.videoid;
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDiRd5_9yRXWjFnMvS8KVH2MS7o8LSSUCM';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = {
+		  videos: [],
+      selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'taco hemingway'}, (videos) => {
-			this.setState({ videos });
+			this.setState({
+			  videos,
+        selectedVideo: videos[0]
+			});
 		});
 
 		console.log(this.state);
@@ -23,6 +30,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video = { this.state.selectedVideo   }/>
 				<VideoList videos = { this.state.videos } />
 			</div>
 		);
